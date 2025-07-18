522f0574983fe2789b5406f1c2adba56730777e5
jaeho0718 <jaeho010718@gmail.com>
2025-07-18 20:44:19 +0900

    Initial Commit

---
+++ BedSolution/BedSolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BedSolutionApp.swift
+//  BedSolution
+//
+//  Created by 이재호 on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BedSolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
