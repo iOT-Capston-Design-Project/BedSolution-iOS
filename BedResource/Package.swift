f48411c4a3253125c30846ec4be351a8a4c8cce6
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2025-07-20 23:26:18 +0900

    Rename Resource module to BedResource

--- BedResource/Package.swift
+++ BedResource/Package.swift
@@ -4,17 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "Resource",
+    name: "BedResource",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Resource",
-            targets: ["Resource"]),
+            name: "BedResource",
+            targets: ["BedResource"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Resource")
+            name: "BedResource")
     ]
 )
--- BedSolution/ContentView.swift
+++ BedSolution/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import Resource
+import BedResource
 
 struct ContentView: View {
     var body: some View {
